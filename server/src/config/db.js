69f6fb4caffacc1daa7f7e405ff6f05e5573a682
Nick-Star-bit <nicolasstar39@gmail.com>
2026-09-15 19:09:37 -0500

    Create db.js

---
+++ server/src/config/db.js
@@ -0,0 +1,7 @@
+EVIDENCIA: GA7-220501096-AA5-EV01 - Diseño y desarrollo de servicios web - caso
+APRENDIZ: Nicolás Stiven Moreno López
+FICHA: 3336195
+PROYECTO FORMATIVO: The Closet 
+
+ENLACE AL REPOSITORIO DE GITHUB:
+https://github.com/Nick-Star-bit/TheCloset_

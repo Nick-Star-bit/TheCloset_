1fd8e5efff71f1f48c1c8952d4392f091cbdb71a
davidsgames4804-collab <davidsgames4804@gmail.com>
2026-09-18 15:12:00 -0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,13 @@
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
-
+const userRoutes = require('./routes/userRoutes');
 // Middleware para procesar JSON
 app.use(express.json());
 
 // Enrutador de autenticación
 app.use('/api/auth', authRoutes);
-
+app.use('/api/users', userRoutes);
 // Puerto de ejecución
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

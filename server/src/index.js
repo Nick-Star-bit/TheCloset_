1e79da2b3df58ec3d057d1e0215cbc37546b6989
Nick-Star-bit <nicolasstar39@gmail.com>
2026-09-18 22:28:51 -0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,22 @@
-// Servidor principal de Express para The Closet 
 const express = require('express');
 const app = express();
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-// Middleware para procesar JSON
+require('dotenv').config();
+
 app.use(express.json());
 
-// Enrutador de autenticación
+// Importación de rutas de todos los integrantes
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const productRoutes = require('./routes/productRoutes');
+
+// Registro de endpoints en la API
 app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-// Puerto de ejecución
+app.use('/api/usuarios', userRoutes);
+app.use('/api/pedidos', orderRoutes);
+app.use('/api/productos', productRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor de The Closet ejecutándose en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
